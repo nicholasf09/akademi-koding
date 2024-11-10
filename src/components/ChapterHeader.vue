b1ddad4bda74bb22f6ea5b2d14e0539f8f3faf2a
Nicholas Fransisco <nicholasfransisco09@gmail.com>
2024-11-10 21:53:54 +0700

    fix: back to course redirect

--- src/components/ChapterHeader.vue
+++ src/components/ChapterHeader.vue
@@ -1,8 +1,13 @@
 <template>
   <div class="text-black p-4 flex items-center fixed w-full justify-between top-0 border-b bg-violet-100 border-gray-700 z-50">
-    <a href="/register" class="bg-violet-500 hover:bg-violet-700 text-white font-bold py-2 px-4 rounded">Back To Courses</a>
+    <button 
+      class="bg-violet-500 hover:bg-violet-700 text-white font-bold py-2 px-4 rounded"
+      @click="goBack"
+    >
+      Back To Course
+    </button>
     <h1 class="text-lg font-semibold mx-8 cursor-pointer">Current Course: {{ moduleTitle }}</h1>
-    <a href="/register" class="bg-violet-500 hover:bg-violet-700 text-white font-bold py-2 px-4 rounded">Go To Projects</a>
+    <a href="/register" class="bg-violet-500 hover:bg-violet-700 text-white font-bold py-2 px-4 rounded">Project Submission</a>
   </div>
   <div class="text-black p-4 flex items-center justify-between fixed border-t bg-violet-100 border-gray-700 w-full bottom-0 z-50">
     <button
@@ -37,6 +42,13 @@ export default {
       type: Boolean,
       required: true
     }
-  }
+  },
+  methods: {
+    goBack() {
+      // Navigate back to the course page without the module part in the URL
+      const courseSlug = this.$route.params.courseSlug;
+      this.$router.push(`/course/${courseSlug}`); // Navigate to /course/{courseSlug}
+    },
+  },
 };
 </script>

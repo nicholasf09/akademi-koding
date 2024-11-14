ef16151213aec326931fa4929f48e13110ab5eef
HeyCW <131667818+HeyCW@users.noreply.github.com>
2024-11-14 16:02:35 +0700

    add create, read, and update course

--- src/components/DataTableCourse.vue
+++ src/components/DataTableCourse.vue
@@ -8,9 +8,19 @@
 
     const searchFilter = ref('');
 
-    const module = ref([]);
+    const module = ref({
+        name: '',
+        slug: '',
+        description: '',
+        link: '',
+        project: ''
+    });
 
     const isModalVisible = ref(false);
+
+    const idModelEdit = ref(0);
+
+    const isEdit = ref(false);
 
     const emit = defineEmits(['create-item']);
 
@@ -18,8 +28,13 @@
         items: {
             type: Array,
             required: true
+        },
+        idCourse: {
+            type: Number,
+            required: true
         }
     })
+
 
     const filteredItems = computed(() => {
         if (searchFilter.value != '') {
@@ -32,8 +47,21 @@
         searchFilter.value = value;
     }
 
-    const editClick = (id) => {
+    const chapterClick = (id) => {
         router.push({'path': `/admin/module/${id}`});
+    }
+
+    const editClick = (id, name, slug, description, link, project) => {
+        isModalVisible.value = true;
+        idModelEdit.value = id;
+
+        module.value.name = name;
+        module.value.slug = slug;
+        module.value.description = description;
+        module.value.link = link;
+        module.value.project = project;
+        isEdit.value = true;
+
     }
 
     const handleCreate = () => {
@@ -42,12 +70,50 @@
 
     const submitCreate = () => {
         const newItem = {
-            id: props.items.length + 1,
-            module: module.value,
+            name: module.value.name,
+            course_id: props.idCourse,
+            slug: module.value.slug,
+            description: module.value.description,
+            link: module.value.link,
+            project: module.value.project
         };
         emit('create-item', newItem);
-        module.value = '';
+        module.value = {
+            name: '',
+            slug: '',
+            description: '',
+            link: '',
+            project: ''
+        };
         isModalVisible.value = false;
+    }
+
+    const submitEdit = () =>{
+        const id = idModelEdit.value;
+        const name = module.value.name;
+        const slug = module.value.slug;
+        const description = module.value.description;
+        const link = module.value.link;
+        const project = module.value.project;
+
+        const updatedItem = {
+            id: id,
+            name: name,
+            slug: slug,
+            description: description,
+            link: link,
+            project: project
+        };
+
+        emit('update-item', updatedItem);
+        isModalVisible.value = false;
+        module.value = {
+            name: '',
+            slug: '',
+            description: '',
+            link: '',
+            project: ''
+        };
     }
 
 </script>
@@ -55,13 +121,35 @@
 <template>
 
     <Modal :isVisible="isModalVisible" @close="isModalVisible = false">
-        <h2 class="text-lg font-semibold mb-5">Create Module</h2>
+        <h2 class="text-lg font-semibold mb-5">{{  }}</h2>
         <div class="flex flex-col mb-5">
             <label class="text-sm font-semibold">Module</label>
-            <input type="text" v-model="module" class="border-2 p-1" />
+            <input type="text" v-model="module.name" class="border-2 p-1" />
         </div>
+
+        <div class="flex flex-col mb-5">
+            <label class="text-sm font-semibold">Slug</label>
+            <input type="text" v-model="module.slug" class="border-2 p-1" />
+        </div>
+
+        <div class="flex flex-col mb-5">
+            <label class="text-sm font-semibold">Description</label>
+            <input type="text" v-model="module.description" class="border-2 p-1" />
+        </div>
+
+        <div class="flex flex-col mb-5">
+            <label class="text-sm font-semibold">Link</label>
+            <input type="text" v-model="module.link" class="border-2 p-1" />
+        </div>
+
+        <div class="flex flex-col mb-5">
+            <label class="text-sm font-semibold">Project</label>
+            <input type="text" v-model="module.project" class="border-2 p-1" />
+        </div>
+
         <div class="flex w-full justify-between">
-            <button @click="submitCreate" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Create</button>
+            <button v-if="!isEdit" @click="submitCreate" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Create</button>
+            <button v-if="isEdit" @click="submitEdit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Edit</button>
         </div>
 
     </Modal>
@@ -72,7 +160,7 @@
             <SearchForm @search="handleSearch" />
 
             <div class="flex items-center justify-end text-sm font-semibold">
-                <input type="text" class="border-2 p-2 rounded-xl shadow-md hover:shadow-lg" value="Data Science">
+                <input type="text" class="border-2 p-2 rounded-xl shadow-md hover:shadow-lg" value="Data Science" disabled>
             </div>
         </div>
 
@@ -82,18 +170,31 @@
                 <tr>
                     <th class="font-semibold py-2 px-4">ID</th>
                     <th class="font-semibold py-2">Module</th>
-                    <th class="font-semibold py-2">Chapters</th>
+                    <th class="font-semibold py-2">Slug</th>
+                    <th class="font-semibold py-2">Description</th>
+                    <th class="font-semibold py-2">Link</th>
+                    <th class="font-semibold py-2">Project</th>
                     <th class="font-semibold py-2 px-4">Edit</th>
+                    <th class="font-semibold py-2 px-4">Chapters</th> 
                 </tr>
             </thead>
             <tbody>
                 <tr v-for="item in filteredItems" :key="item.id">
                     <td class="font-semibold py-2 px-4">{{ item.id }}</td>
-                    <td class="font-semibold py-2">{{ item.module }}</td>
-                    <td class="font-semibold py-2">{{ item.chapters }}</td>
+                    <td class="font-semibold py-2">{{ item.name }}</td>
+                    <td class="font-semibold py-2">{{ item.slug }}</td>
+                    <td class="font-semibold py-2">{{ item.description }}</td>
+                    <td class="font-semibold py-2">{{ item.link }}</td>
+                    <td class="font-semibold py-2">{{ item.project }}</td>
                     <td class="font-semibold py-2 px-4">
-                        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" @click="editClick(item.id)">
+                        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" @click="editClick(item.id, item.name, item.slug, item.description, item.link, item.project)">
                             Edit
+                        </button>
+                    </td>
+
+                    <td class="font-semibold py-2 px-4">
+                        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" @click="chapterClick(item.id)">
+                            Chapters
                         </button>
                     </td>
                     
--- src/services/module.service.js
+++ src/services/module.service.js
@@ -17,6 +17,22 @@ export const getModules = (callback) => {
     });
 };
 
+export const getModuleByCourseId = async (courseId) => {
+  const token = getCookies("token");
+  try {
+    const response = await axios.get(`http://localhost:3000/module/course/${courseId}`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      }
+    });
+
+    return response.data; 
+  } catch (error) {
+    console.error("Error fetching module by course id:", error);
+    throw error;
+  }
+};
+
 export const getModuleBySlug = async (slug) => {
   const token = getCookies("token");
   try {
@@ -30,4 +46,41 @@ export const getModuleBySlug = async (slug) => {
     console.error("Error fetching module by slug:", error);
     throw error;
   }
+};
+
+
+export const addModule = (moduleData, callback) => {
+  const token = getCookies("token");
+  axios.post("http://localhost:3000/add/module", moduleData, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    }
+  })
+    .then((res) => {
+      callback(res.data);
+    })
+    .catch((err) => {
+      console.log("Error adding module:", err);
+    });
+};
+
+export const updateModule = (moduleData, courseId, callback) => {
+  const token = getCookies("token");
+
+  const dataToSend = {
+    ...moduleData,
+    courseId, 
+  };
+
+  axios.post("http://localhost:3000/update/module", dataToSend, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    }
+  })
+    .then((res) => {
+      callback(res.data);
+    })
+    .catch((err) => {
+      console.log("Error editing module:", err);
+    });
 };

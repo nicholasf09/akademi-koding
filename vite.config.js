220536bc9530ab23bfbaa09b15147b6020723ada
HeyCW <131667818+HeyCW@users.noreply.github.com>
2024-11-19 12:04:18 +0700

    Tambahin middleware

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,43 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import jwt from 'jsonwebtoken';
+
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    {
+      name: 'auth-middleware',
+      configureServer(server) {
+        server.middlewares.use((req, res, next) => {
+          // Daftar route yang dilindungi
+          const protectedRoutes = [
+            '/course', '/admin', '/add/course', '/add/module', '/add/chapter', '/add/question'
+          ];
+
+          // Cek apakah URL yang diminta adalah route yang dilindungi
+          if (protectedRoutes.includes(req.url)) {
+            const token = req.headers.cookie
+              ? req.headers.cookie.split('; ').find(row => row.startsWith('token='))?.split('=')[1]
+              : null;
+            const verify = jwt.verify(token, 'Nikf_Mbek_Mb_ganteng123');
+            
+            if (!token) {
+              // Kirim respon 401 jika token tidak ada
+              res.statusCode = 401;
+              res.setHeader('Content-Type', 'application/json');
+              res.end(JSON.stringify({ error: 'Unauthorized' }));
+              return;
+            }
+          }
+
+          // Lanjutkan jika token valid atau route tidak dilindungi
+          next();
+        });
+      },
+    },
   ],
   resolve: {
     alias: {

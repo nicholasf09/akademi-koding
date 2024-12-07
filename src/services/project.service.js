e89f0703dc51aa76a984eb45adf67dbb0d7e6848
Nicholas Fransisco <nicholasfransisco09@gmail.com>
2024-12-07 23:38:02 +0700

    fix: retrieve deleted func

--- src/services/project.service.js
+++ src/services/project.service.js
@@ -1,19 +1,64 @@
 import axios from 'axios';
 import getCookies from '../hooks/getCookies';
 
-export const getProjectDetails = (idModule, callback) => {
+export const getProjectDetails = (idModule) => {
   const token = getCookies("token");
   const API_ENDPOINT = import.meta.env.VITE_API_ENDPOINT;
 
-  axios.get(`${API_ENDPOINT}/projects/${idModule}`, {
+  return axios.get(`${API_ENDPOINT}/projects/${idModule}`, {
     headers: {
       Authorization: `Bearer ${token}`,
-    }
+    },
   })
-    .then((res) => {
-      callback(res.data);
-    })
+    .then((res) => res.data)
     .catch((err) => {
       console.log("Error fetching project details:", err);
+      throw err; // Lempar error untuk penanganan di tempat lain
     });
 };
+
+export const updateProject = (id, comment, score) => {
+  const token = getCookies("token");
+  const API_ENDPOINT = import.meta.env.VITE_API_ENDPOINT;
+
+  return axios.post(
+    `${API_ENDPOINT}/project/update`, // Tetap menggunakan POST
+    { id, comment, score }, // Data pembaruan dikirim sebagai body JSON
+    {
+      headers: {
+        Authorization: `Bearer ${token}`, // Sertakan token autentikasi
+        "Content-Type": "application/json", // Set tipe konten ke JSON
+      },
+    }
+  )
+    .then((res) => res.data) // Return data dari respons
+    .catch((err) => {
+      console.error("Error updating project:", err);
+      throw err; // Lempar error untuk penanganan lebih lanjut
+    });
+};
+
+export const submitProject = (idUser, idModule, project) => {
+  const token = getCookies("token");
+  const API_ENDPOINT = import.meta.env.VITE_API_ENDPOINT;
+
+  return axios.post(
+    `${API_ENDPOINT}/project/submit`, // Endpoint untuk submit proyek
+    { idUser, idModule, project }, // Data yang dikirim dalam body
+    {
+      headers: {
+        Authorization: `Bearer ${token}`, // Token untuk autentikasi
+        "Content-Type": "application/json", // Tipe konten JSON
+      },
+    }
+  )
+    .then((res) => res.data) // Return data dari respons
+    .catch((err) => {
+      console.error("Error submitting project:", err);
+      throw err; // Lempar error untuk penanganan lebih lanjut
+    });
+};
+
+
+
+
